84f15e2ac83da0dae3575f2f679ba43e6a1863bb
Thiago Barreto Rodrigues <tbarreto585@gmail.com>
2024-07-30 11:08:06 -0400

    Home and header are responsive.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,43 @@
+import { faAddressBook, faFileAlt, faHome, faInfoCircle, faProjectDiagram, faTasks } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from 'react-router-dom'
 import { ModeToggle } from './mode-toggle'
 
 export function Header() {
   return (
-    <header className='w-full flex items-center justify-center p-4 gap-5 absolute top-0'>
+    <header
+      className='w-full flex items-center justify-center flex-col lg:flex-row p-4 gap-5 absolute top-0 -left-32 md:-left-80 lg:-left-12'
+    >
       <ModeToggle />
-      <nav className='rounded-full bg-zinc-950 text-zinc-50 dark:bg-zinc-100 dark:text-zinc-950 px-4 py-2 flex gap-20 items-center'>
-        <div className='flex gap-8'>
-          <a href='/'>Home</a>
-          <a href='/about'>Sobre</a>
-          <a href='/services'>Serviços</a>
+      <nav className='rounded-full bg-zinc-950 text-zinc-50 w-10 h-auto lg:w-auto lg:h-auto dark:bg-zinc-100 dark:text-zinc-950 px-4 py-1 flex lg:gap-16 items-center flex-col lg:flex-row gap-4 lg:px-2 lg:py-2'>
+        <div className='flex lg:gap-2 flex-col gap-2 lg:flex-row'>
+          <a href='/' className='py-2 rounded-full lg:hover:bg-violet-800 hover:text-zinc-50 flex items-center transition-all duration-300 justify-center px-2 lg:px-6'>
+            <FontAwesomeIcon icon={faHome} className='block lg:hidden' />
+            <span className='hidden lg:block'>Home</span>
+          </a>
+          <a href='/about' className='py-2 rounded-full lg:hover:bg-violet-800 hover:text-zinc-50 flex items-center transition-all duration-300 justify-center px-2 lg:px-6'>
+            <FontAwesomeIcon icon={faInfoCircle} className='block lg:hidden' />
+            <span className='hidden lg:block'>Sobre nós</span>
+          </a>
+          <a href='/services' className='py-2 rounded-full lg:hover:bg-violet-800 hover:text-zinc-50 flex items-center transition-all duration-300 justify-center px-2 lg:px-6'>
+            <FontAwesomeIcon icon={faTasks} className='block lg:hidden' />
+            <span className='hidden lg:block'>Serviços</span>
+          </a>
         </div>
-        <p>CromoCode</p>
-        <div className='flex gap-8'>
-          <a href='/resume'>Resumo</a>
-          <a href='/projects'>Projetos</a>
-          <a href='/contacts'>Contatos</a>
+        <Link to={'/'} className='hidden lg:block'>CromoCode</Link>
+        <div className='flex lg:gap-2 flex-col gap-2 lg:flex-row'>
+          <a href='/resume' className='py-2 rounded-full lg:hover:bg-violet-800 hover:text-zinc-50 flex items-center transition-all duration-300 justify-center px-2 lg:px-6'>
+            <FontAwesomeIcon icon={faFileAlt} className='block lg:hidden' />
+            <span className='hidden lg:block'>Resumo</span>
+          </a>
+          <a href='/projects' className='py-2 rounded-full lg:hover:bg-violet-800 hover:text-zinc-50 flex items-center transition-all duration-300 justify-center px-2 lg:px-6'>
+            <FontAwesomeIcon icon={faProjectDiagram} className='block lg:hidden' />
+            <span className='hidden lg:block'>Projetos</span>
+          </a>
+          <a href='/contacts' className='py-2 rounded-full lg:hover:bg-violet-800 hover:text-zinc-50 flex items-center transition-all duration-300 justify-center px-2 lg:px-6'>
+            <FontAwesomeIcon icon={faAddressBook} className='block lg:hidden' />
+            <span className='hidden lg:block'>Contatos</span>
+          </a>
         </div>
       </nav>
     </header>
--- src/pages/(cromoCode)/home/home.tsx
+++ src/pages/(cromoCode)/home/home.tsx
@@ -8,22 +8,27 @@ export function Home() {
     <LayoutMain>
       <div className="h-screen flex items-center w-full overflow-hidden justify-around flex-col">
         <motion.h1
-          className="text-3xl"
+          className="text-lg md:text-xl lg:text-3xl"
           initial={{ scale: 0, opacity: 0 }}
           animate={{ scale: 2, opacity: 1 }}
           exit={{ opacity: 0 }}
         >CromoCode</motion.h1>
-        <p className='absolute left-52 top-96'>
+        <motion.p
+          className='absolute left-10 top-0 hidden w-[200px]'
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
           <Quote />
-          adadasdasdasdsdasdasdasdasdasdasdasdasdasdasdasdas
-        </p>
+          O design excepcional do produto garante o sucesso do nosso site. Altamente recomendado
+        </motion.p>
         <div className='relative'>
           <motion.div
             className='rounded-full bg-violet-800 flex absolute -bottom-[600px] items-center justify-center h-[700px] w-[700px]'
             initial={{ left: 0 }}
-            animate={{ left: -300 }}
+            animate={{ left: -350 }}
           >
-            <img src={ImgPersonConference} alt="" className='h-80 absolute -top-36' />
+            <img src={ImgPersonConference} alt="" className='h-56 absolute -top-24 md:h-80 md:-top-36' />
             <motion.div
               className='bg-zinc-200 rounded-full w-auto p-2 bg-opacity-70 flex gap-2 items-center text-zinc-950 -mt-60'
               initial={{ opacity: 0, y: 20 }}
