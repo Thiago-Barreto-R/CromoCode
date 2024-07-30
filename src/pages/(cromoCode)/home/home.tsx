0f9bb06a21b10e8d034b379804f7772b98006448
Thiago Barreto Rodrigues <tbarreto585@gmail.com>
2024-07-30 16:51:47 -0400

    View layout.

--- src/pages/(cromoCode)/home/home.tsx
+++ src/pages/(cromoCode)/home/home.tsx
@@ -12,7 +12,7 @@ export function Home() {
           <motion.img
             src={logoOfc}
             alt=""
-            className='h-20 relative'
+            className='h-10 relative'
             initial={{ scale: 0, opacity: 0, left: 80 }}
             animate={{ scale: 2, opacity: 1, left: -30 }}
             exit={{ opacity: 0 }}
@@ -23,21 +23,20 @@ export function Home() {
             animate={{ top: 310, left: 490, opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <p className='hidden 2xl:block'>
-              Com uma equipe de especialistas apaixonados por tecnologia, o CromoCode oferece desenvolvimento de software personalizado e consultoria técnica.
-              Estamos aqui para elevar o seu negócio a novos patamares!
-            </p>
+            Com uma equipe de especialistas apaixonados por tecnologia, o CromoCode oferece desenvolvimento de software personalizado e consultoria técnica.
+            Estamos aqui para elevar o seu negócio a novos patamares!
+
           </motion.p>
         </div>
         <div className='relative'>
           <motion.div
             className='rounded-full bg-blue-800 flex absolute -bottom-[600px] items-center justify-center h-[700px] w-[700px]'
             initial={{ left: 0 }}
-            animate={{ left: -370 }}
+            animate={{ left: -350 }}
           >
-            <img src={ImgPersonConference} alt="" className='h-56 absolute -top-24 md:h-80 md:-top-36' />
+            <img src={ImgPersonConference} alt="" className='h-48 absolute -top-16 md:h-80 md:-top-36' />
             <motion.div
-              className='bg-zinc-200 rounded-full w-auto p-2 flex gap-2 items-center text-zinc-50 -mt-60 shadow-lg  bg-opacity-20'
+              className='bg-zinc-200 rounded-full w-auto p-2 flex gap-2 items-center text-zinc-50 -mt-72 shadow-lg  bg-opacity-20'
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5 }}
@@ -45,7 +44,7 @@ export function Home() {
               <a
                 href="https://wa.me/5592991037056"
                 target='_blank'
-                className='bg-blue-800 py-2 px-5 rounded-full text-zinc-50 flex gap-1 items-center transition-all duration-300 hover:scale-105'
+                className='bg-blue-800 py-2 px-3 rounded-full text-zinc-50 flex gap-1 items-center transition-all duration-300 hover:scale-105'
               >
                 Fale Conosco
                 <MoveUpRight size={16} className='dark:text-white' />
